7b0c03c3bda25e91c6602f527aa11ef0a3d3ec08
Scott Forman <sjforman@gmail.com>
2013-11-20 12:35:18 -0500

    added social sharing with addthis

---
+++ source/javascripts/vendor/addthis.js
@@ -0,0 +1,14 @@
+<!-- AddThis Smart Layers BEGIN -->
+  addthis.layers({
+    'theme' : 'transparent',
+    'share' : {
+      'position' : 'left',
+      'numPreferredServices' : 5
+    }, 
+    'follow' : {
+      'services' : [
+        {'service': 'twitter', 'id': 'letterprojectny'}
+      ]
+    }   
+  });
+<!-- AddThis Smart Layers END -->
