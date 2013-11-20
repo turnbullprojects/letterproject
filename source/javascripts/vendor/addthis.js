f92e4549a90cfdac24f097cc6fb410ad615457c7
Scott Forman <sjforman@gmail.com>
2013-11-20 12:40:22 -0500

    removed comments from addthis.js to satisfy complaints of middleman build

--- source/javascripts/vendor/addthis.js
+++ source/javascripts/vendor/addthis.js
@@ -1,14 +1,12 @@
-<!-- AddThis Smart Layers BEGIN -->
-  addthis.layers({
-    'theme' : 'transparent',
-    'share' : {
-      'position' : 'left',
-      'numPreferredServices' : 5
-    }, 
-    'follow' : {
-      'services' : [
-        {'service': 'twitter', 'id': 'letterprojectny'}
-      ]
-    }   
-  });
-<!-- AddThis Smart Layers END -->
+addthis.layers({
+  'theme' : 'transparent',
+  'share' : {
+    'position' : 'left',
+    'numPreferredServices' : 5
+  }, 
+  'follow' : {
+    'services' : [
+      {'service': 'twitter', 'id': 'letterprojectny'}
+    ]
+  }   
+});
